2453418ef0fa7a90a0964182f442761a33e6bc47
fa-m <info@fmalik.de>
2017-10-21 12:16:23 +0200

    initial commit

---
+++ company_cooking/imports/api/links/links.js
@@ -0,0 +1,6 @@
+// Definition of the links collection
+
+import { Mongo } from 'meteor/mongo';
+
+export const Links = new Mongo.Collection('links');
+export const Users = new Mongo.Collection('users');
